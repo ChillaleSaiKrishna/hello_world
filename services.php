28dbf9afca6183613f7dd08bcc32093eeedbdb71
ChillaleSaiKrishna <79957936+ChillaleSaiKrishna@users.noreply.github.com>
2021-03-03 15:28:49 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,247 @@
+<?php include("header.php"); ?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Services</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Services</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about-us" class="about-us">  
+      <div class="container">
+
+        <div class="row no-gutters">
+            <img  class="col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start" src= "assets/img/development-company.jpg">
+          <!-- <div class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start" data-aos="fade-right"></div> -->
+          <div class="col-xl-7 ps-0 ps-lg-5 pe-lg-1 d-flex align-items-stretch">
+            <div class="content d-flex flex-column justify-content-center">
+              <!-- <h3  class = "titleColor" style = "text-align :center" data-aos="fade-up">Who We Are</h3> -->
+              <p data-aos="fade-up">
+             <b> Biometric security, authentication and identification </b>at the forefront of world standards is fully integrated into the SreeTech™ Operating System.
+              <br>
+              <b>Breakthrough Semantics that eliminate the need for coding </b>and uses natural language to initiate queries. All data BIG and small, can be interrogated, analysed and engineered to provide immediate answers.
+              <br>
+              <b>Blockchained Transactions.  SreeTech™ delivers a blockchained plug-play solution </b>to the rapidly expanding requirements of government, cities, large enterprise and SME businesses incubation.
+            </p>
+       
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Our Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our <strong>Services</strong></h2>
+        </div>
+
+        <div class="row">
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/bioatm.png" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">SELF-SERVICE MACHINES/KIOSKS</h4>
+                <p>Fed-up with old fashion, ineffectual and complicated kiosks? We are a leading provider of interactive touchscreen kiosks, with built-in multi-factor face, finger-print and other biometric authentications.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/beacon-notification.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">BEACONS/RFID BASED SOLUTIONS</h4>
+                <p>Whether you want to track high value assets in a large assembly or exhibition area or manage their productivity in a hospital, we have ready-to-use tagging solutions. </p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/Digital_Signage_3b4.jfif" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">DIGITAL SIGNAGE MANAGEMENT</h4>
+                <p>Communication today is about visualization and live data. Our Digital signage with built-in camera is a smart visual communication solution for businesses to measure audience engagement.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/3-array.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">DEDUPE ANALYSIS</h4>
+                <p>Our dedupe analysis engines capture duplicate bank customers or telecom subscribers and more. They are also plug-and-play and offer real-time analysis as well. Talk to us to see how this technology can help you.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/biometricw.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">BIOMETRIC WEIGHMENTS</h4>
+                <p>Fed-up with old, ineffectual and complicated Weighment system? We are a leading provider of Biometric Weighment solutions, with built-in face and finger print authentications.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/energy_sector_part1.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">HAND-HELD DEVICES</h4>
+                <p>Want to take your business to customer door steps? We are the provider of Hand-held devices with built-in face, finger print and other biometric authentications.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/facial-recognition-data-points.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">BIOMETRIC AUTH, VERIFICATION & VALIDATION</h4>
+                <p>Our Biometric solutions can be deployed in many contexts. From tracking Wanted lists to identifying your customers to attendance management to access control and more. Talk to us to find out how this contact-less technology can do wonders for your business.  </p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/indust4.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">INDUSTRY 4.0 IOT</h4>
+                <p>Enable remote monitoring, analyze trends, or save gillions? We help you to explore where your devices seamlessly interact with each other to gather, store, and process data. You may boost your productivity, generate new revenue streams, enhance security or radically transform your service support - all of this becomes possible with IoT.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div >
+                <img style = "margin-top:2%;height: 10%;width:70%" src="assets/img/aicog.jpg" class="img-fluid" alt="">
+             
+              </div>
+              <div class="member-info">
+                <h4  style = "color:#f03c02;">AI & COGNITIVE ANALYTICS</h4>
+                <p>From Machine Learning to Natural Language Processing, our plug-and-play technology services in this area can propel you ahead of your competition. Talk to us.</p>
+              </div>
+            </div>
+          </div>
+         
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->
+
+    <!-- ======= Our Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our <strong>Skills</strong></h2>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">Data Science <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Artificial Intelligence <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">AWS <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">Python <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Machine Learning<i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">IOT <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Skills Section -->
+    <?php include("footer.php"); ?>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+ </body>
+
+</html>
